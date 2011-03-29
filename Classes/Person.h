ab3c34236ba06bffc1b8b907f1d3873982fe5fad
Raymond Huynh <rhuy238@chil16.orchestra.cse.unsw.edu.au>
2011-03-29 15:50:39 +1100

    Person

---
+++ Classes/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  Class1
+//
+//  Created by Raymond Huynh on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Person : NSObject {
+
+}
+
+@end
